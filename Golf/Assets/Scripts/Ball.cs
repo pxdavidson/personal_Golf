b8043be24b09319228dd7a7c024a827f041b4a8b
Phil Davidson <pxdavidson@msn.com>
2020-05-18 23:48:45 +0100

    Moving to 2D
    Aaltered scene setup for a 2.5D game.
    Added basic materials for debug purposes
    Update ball.cs to use Transformvector() instead of transform.forward()

---
+++ Golf/Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Variables
+    float xAngle = 1f;
+    float yAngle = 0f;
+    float zAngle = 0f;
+
+    // Cache
+    [SerializeField] float thrust = 1.0f;
+    Rigidbody ballRigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRigidbody = GetComponent<Rigidbody>();
+        ballRigidbody.AddForce(transform.TransformVector(xAngle,yAngle,zAngle) * thrust);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
